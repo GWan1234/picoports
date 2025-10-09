83926af425db55907a9faadfe733bfa70fd2e2aa
Gero Schwäricke <gero.schwaericke@sevenlab.de>
2025-10-09 11:38:16 +0200

    Add missing include for enum

--- src/main.h
+++ src/main.h
@@ -5,6 +5,8 @@
 #ifndef _PP_MAIN_H_
 #define _PP_MAIN_H_
 
+#include "dln2.h"
+
 void send_message_delayed(uint16_t cmd, uint16_t echo, enum dln2_handle handle,
 			  uint8_t *data, uint16_t data_len);
 
